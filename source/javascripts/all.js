3731888d93f2fc6ec2cdd07fcd8acaf960f6c128
Sergio Marin <higher.vnf@gmail.com>
2013-04-10 16:55:59 -0430

    agregando pagina de muestra de todos los iconos disponibles

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
+//= require_tree "./plugins"
 //= require_tree .
+
+(function($, window, document, undefined) {
+  
+  $(function() {
+    $('pre.codehighlight').each(function(i, e) { hljs.highlightBlock(e) });
+  })
+
+})(window.jQuery, window, document);
+
+
