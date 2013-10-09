9ba7b819e17037b44f6132333c263377a74051e0
Sergio marin <higher.vnf@gmail.com>
2013-10-08 20:38:02 -0430

    responsive menu

--- source/javascripts/octagon.js
+++ source/javascripts/octagon.js
@@ -6,6 +6,11 @@
   $(function() {
     $('pre').each(function(i, e) { hljs.highlightBlock(e) });
     $(".menu.scroller a").smoothscrolling();
+    $(".menu-responsive-toggle").on("click", function() {
+    	var $this = $(this),
+    		target = $this.data('target');
+    	$(target).toggleClass("active");
+    });
   })
 
 })(window.jQuery, window, document);
